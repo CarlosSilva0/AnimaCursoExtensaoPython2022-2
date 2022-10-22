d85e5df1889f53c31269231f85fdd536a28774f6
CarlosSilva0 <>
2022-10-22 16:18:13 +0000

    Voltei a o que aprendi em extensão e transferi o treino para outro

--- main.py
+++ main.py
@@ -11,29 +11,3 @@ print(nome)
 print(f'Minha idade é {idade} anos')
 #quando eu quiser exibir 'Meu nome é... e tenho... anos, trocando pelas variáveis nome e idade
 print ('Meu nome é {} e tenho {} anos'.format(nome, idade)) 
-
-palavra = 'Banana Naninca'
-contagem = 0 
-for letter in palavra:
-  if letter == 'a':
-    contagem = contagem + 1
-    print (contagem)   
-
-word= 'Calma'
-print (word) 
-
-fruta = 'Morango'
-print (fruta[:3])
-
-indice = 0
-while indice < len(fruta):
-  letra = fruta [indice]
-  print(letra)
-  indice = indice + 1
-
-for char in fruta:
-  print (char)
-
-l = 'Santos Futebol'
-print (l[0:6])
-print (l[7:14])
