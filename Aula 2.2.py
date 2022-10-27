a429a4290403669d1eb394f6045e1ef0daf1d075
CarlosSilva0 <116295579+CarlosSilva0@users.noreply.github.com>
2022-10-27 01:23:55 +0000

    Final da Aula 2

---
+++ Aula 2.2.py
@@ -0,0 +1,12 @@
+# Uma nota de (0 a 10) se o aluno tirou 10 escreva "Você é bichão mesmo
+
+nome =input ('Escreva seu nome: ')
+nota =float(input('Digite sua nota: ')) 
+
+if nota ==10:
+  print(f'{nome}, você é o bichão mesmo')
+elif nota>=6 and nota<10:
+  print(f'{nome}, você é mediano')
+else:
+  print ('BURRO')
+  
